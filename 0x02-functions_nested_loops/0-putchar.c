90a86610ff1295512a2079de33807d4c547eccea
danny2633 <dsamuel47@yahoo.com>
2022-12-15 03:03:51 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include "main.h"
-
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: On success 1.
- * On error, 1 is returned, and errno is set appropriately
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+int main(void)
 
 {
-	return (write(1, 6c 1)):
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
 }
