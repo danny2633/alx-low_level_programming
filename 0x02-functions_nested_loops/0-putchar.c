7f0f694f47daef9857c07f3f4bdadbd52697a587
danny2633 <dsamuel47@yahoo.com>
2023-01-02 06:08:31 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *  main - print _putchar
+ *  is written by mbah
+ *  Return: 0
  */
-
 int main(void)
 
 {
@@ -17,7 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
-
 }
