7df8e3ab78eda3dc577c013b06660a449a10ae84
danny2633 <dsamuel47@yahoo.com>
2023-01-02 06:14:57 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -10,7 +10,8 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);								int add(int, int);
+void times_table(void);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
